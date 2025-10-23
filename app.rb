4329da39cb26895219f0a00e8d6a725586002409
Adolfo Nava <adolfonava1990@gmail.com>
2025-10-23 21:41:30 +0000

    In Meeting Notes

--- app.rb
+++ app.rb
@@ -1,6 +1,22 @@
 require 'net/http'
 require 'json'
-uri = URI("https://jsonplaceholder.typicode.com/posts/")
+
+
+# ask user for a pokemon name
+puts "Please enter the name of your favorite pokemon:"
+
+# store the name in a variable
+name = gets.chomp
+
+# then fetch information about that pokemon
+uri = URI("https://pokeapi.co/api/v2/pokemon/#{name}")
 response = Net::HTTP.get(uri)
-data = JSON.parse(response)
-puts data
+
+if response == "Not Found"
+  puts "Pokemon not found. Try again"
+  return
+else
+  data = JSON.parse(response)
+  # print information (eg image url front_shiny)
+  puts data.dig("sprites","front_shiny")
+end
